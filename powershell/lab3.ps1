28928a29c4def61aed88ad9e33fc37f7983f8691
bish0p123 <73266609+bish0p123@users.noreply.github.com>
2021-11-22 16:52:56 -0500

    lab3
    lab3

---
+++ powershell/lab3.ps1
@@ -0,0 +1,7 @@
+get-ciminstance win32_networkadapterconfiguration | where-object ipenabled | 
+format-table @{n="Description" ; width = 15 ; e={$_.description}},
+@{n="Index" ; width = 20 ; e={$_.index}},
+@{n="IPAddress" ; width = 20 ; e={$_.ipaddress}},
+@{n="IPSubnet" ; width = 20 ; e={$_.ipsubnet}},
+@{n="DNSDomain" ; width = 20 ; e={$_.DNSDomain}},
+@{n="DNSServer" ; width = 20 ; e={$_.DNSServerSearchOrder}}
